98967df7d78b85056e2e25b51f7f85a00919e2d3
Margarita <Margarita>
2024-11-02 13:29:11 +0300

    add: forarray

---
+++ forarrau.ts
@@ -0,0 +1,7 @@
+const arraySitys = ['Москва', 'Брест','Могилев','Милан', 'Париж']
+for (let x=0; x< arraySitys.length; x++){
+  if  (arraySitys[x] === 'Брест'){
+    console.log (`в данном массиве по индексу ${x} находится город ${arraySitys[x]}`)
+  }
+    
+}
