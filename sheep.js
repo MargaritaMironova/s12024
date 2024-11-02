6c70f46e6245c51aefc7a319678b6646b083f018
Margarita <Margarita>
2024-11-02 13:52:05 +0300

    add: sheep.js

---
+++ sheep.js
@@ -0,0 +1,8 @@
+const sheep=[0,1,1,0]
+const spot=[]
+for (x=0;x<4; x++){
+    if (sheep[x]===0) {
+spot.push(sheep [x])
+    }
+}
+console.log (`В массиве ${sheep}находится ${spot.length} белые овцы`)
