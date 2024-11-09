c27ee8fca35497b750e3fef6a553ab2cdd89b657
Margarita <Margarita>
2024-11-09 13:14:56 +0300

    function_multiplay.js

---
+++ function_multiplay.js
@@ -0,0 +1,11 @@
+function multiplay(...parauns) {
+    let result = 1
+    for (i of parauns) {
+        result = result * i
+    }
+    return result
+
+}
+
+
+console.log(multiplay(1 , 2 , 3 , 4 , 5 , 6 , 7 ,8));
