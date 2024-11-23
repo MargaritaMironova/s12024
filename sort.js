481b0982bda2c28199fb0268ebbfc4b0d18e49fe
Margarita <Margarita>
2024-11-23 13:31:41 +0300

    sort flowers

---
+++ sort.js
@@ -0,0 +1,5 @@
+const flowers = ['розы','тюльпаны','лилии ','хризантем','ромашки','васильки','гвоздики' ];
+const result =  flowers.sort()
+console.log (result);
+const result2 =flowers.sort().reverse();
+console.log (result2);
